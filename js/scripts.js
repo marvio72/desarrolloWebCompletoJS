e2eee085d2a9411edbc7b2541c660540e70eaea4
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-02-24 01:31:52 -0600

    Object Literal Enhacement

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,19 @@
-// Destructuring
+//Object Literal Enhacement
 
-const cliente = {
-    nombre: 'Alejandra',
-    tipo  : 'Premium',
-    datos: {
-        ubicacion: {
-            ciudad: 'México',
-            pais: 'México'
-        },
-        cuenta: {
-            desde: 12-12-2012,
-            saldo: 4000
-        }
-    },
-    gustos: {
-        musica:['trance','rock']
-    }
+const banda = 'Metallica',
+      genero = 'Heavy Metal',
+      canciones = ['Master of Puppets','Seek & Destroy','Enter Sandman'];
+
+//Forma anterior
+const metallica ={
+    banda: banda,
+    genero: genero,
+    canciones: canciones
 }
-// Ver el contenido del objeto
-console.log(cliente);
 
-//mostrar el valor de cada elemento forma antigua
-const nombreCliente = cliente.nombre,
-      tipoCliente = cliente.tipo,
-      ubicacionCliente = cliente.datos.ubicacion;
+console.log(metallica);
 
-console.log(ubicacionCliente);
+//Nueva forma
+const metallica1 = {banda,genero,canciones}
 
-//nueva manera de mostrar los valores de cada elemento
-console.log('              Nueva manera');
-let {nombre,tipo} = cliente;
-let {datos: {ubicacion}} = cliente;
-console.log(`${nombre} ${tipo}`);
-console.log(ubicacion);
-
-console.log('Mostrar ciudad');
-let { datos: { ubicacion: {ciudad} } } = cliente;
-console.log(ciudad);
-
-console.log('Extraer el saldo');
-let {datos:{cuenta:{saldo}}} = cliente;
-console.log(saldo);
-
-let {gustos: musica} = cliente;
-
-
-console.log(musica);
+console.log(metallica1);
